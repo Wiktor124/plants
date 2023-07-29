9cace1d76ee290a31c85c47eaab2405cf4bc339a
Cetav-B6-14 <106450277+jasonan99@users.noreply.github.com>
2023-07-29 11:51:34 -0600

    form

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,23 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        'eslint:recommended',
-        'eslint-config-prettier'
-    ],
-    "overrides": [
-        {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "rules": {
-    }
-}
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: ['eslint:recommended', 'eslint-config-prettier'],
+	overrides: [
+		{
+			env: {
+				node: true,
+			},
+			files: ['.eslintrc.{js,cjs}'],
+			parserOptions: {
+				sourceType: 'script',
+			},
+		},
+	],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	rules: {},
+};
--- README.md
+++ README.md
@@ -1,10 +1,17 @@
 # To run this project
 
 1. Dependencies
- - `npm install`
+
+- `npm install`
+
 2. Linter
+
 - `npm run lint`
+
 3. Formatter
+
 - `npm run format`
+
 4. Modify styles
+
 - `npm run sass`
