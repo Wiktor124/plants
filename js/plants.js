193bf326fdc51a4d8a5c46af8356ecae85d8303a
JasonPorras2700 <140833791+JasonPorras2700@users.noreply.github.com>
2023-08-03 22:05:21 -0600

    add dates card

--- js/plants.js
+++ js/plants.js
@@ -1,17 +1,22 @@
-const plantsForm = [...document.querySelector('#plantForm')];
-
+// main.js
 class PlantsBuilder {
   constructor() {
     this.plant = {};
   }
 
-  addAttribute(attrName) {
-    this[attrName] = value => {
-      if (value) {
+  addAttribute(attrName, value) {
+    if (value) {
+      if (attrName === 'extra_elements') {
+        if (this.plant[attrName]) {
+          this.plant[attrName].push(value);
+        } else {
+          this.plant[attrName] = [value];
+        }
+      } else {
         this.plant[attrName] = value;
-        return this;
       }
-    };
+      return this;
+    }
   }
 
   build() {
@@ -19,28 +24,42 @@ class PlantsBuilder {
   }
 }
 
-let elections;
-const handleOptions = e => {
+function printPlantData(plantConfig) {
+  const card = document.getElementById('card');
+  card.innerHTML = `
+      <h3 class="card__title">Plant Recommendation</h3>
+      <div class="card__content">
+          <p>Name: ${plantConfig.placement || ''}</p>
+          <p>Sunlight: ${plantConfig.sunlight || ''}</p>
+          <p>Pets: ${plantConfig.pets || ''}</p>
+          <p>Watering: ${plantConfig.watering || ''}</p>
+          <p>Style: ${plantConfig.style || ''}</p>
+          <p>Extras: ${plantConfig.extra_elements ? plantConfig.extra_elements.join(', ') : ''}</p>
+      </div>
+  `;
+}
+
+function handleOptions(e) {
   e.preventDefault();
   const plant = new PlantsBuilder();
 
-  for (let i = 0; i < plantsForm.length; i++) {
-    if (plantsForm[i].checked) {
-      const attributes = [];
-      attributes.push(plantsForm[i].name);
-
-      attributes.forEach(attr => plant.addAttribute(attr));
-
-      elections = plant[plantsForm[i].name](plantsForm[i].value).build();
+  const plantsForm = document.getElementById('plantForm');
+  for (let i = 0; i < plantsForm.elements.length; i++) {
+    const input = plantsForm.elements[i];
+    if ((input.type === 'radio' || input.type === 'checkbox') && input.checked) {
+      plant.addAttribute(input.name, input.value);
     }
   }
 
-  console.log(elections);
-};
+  const plantConfig = plant.build();
+  console.log(plantConfig);
+
+  // Imprimir datos
+  printPlantData(plantConfig);
+}
 
 function initPlantsBuilder() {
-  document
-    .querySelector('#plantForm')
-    .addEventListener('submit', handleOptions);
+  document.getElementById('plantForm').addEventListener('submit', handleOptions);
 }
+
 export default initPlantsBuilder;
