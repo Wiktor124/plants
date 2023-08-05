0f208d9c10ffa865164f196f7e4b9b04b5d4367b
Víctor Peña Ramírez <victortkd88@gmail.com>
2023-08-04 20:13:56 -0600

    new config

--- config.js
+++ config.js
@@ -1,33 +1,42 @@
 const plantsOptions = {
 	placement_answer: {
-		inside_lot_indirect: {
-			yes: 'https://res.cloudinary.com/diplphrxm/image/upload/v1690692069/samples/plants/plant-aglaonema_gpsqr7.png',
-			no: 'https://res.cloudinary.com/diplphrxm/image/upload/v1690692069/samples/plants/plant-monstera_zbntgh.png',
-		},
 		inside_indirect: {
-			yes: 'https://res.cloudinary.com/diplphrxm/image/upload/v1690692068/samples/plants/plant-sansevieria_mnhug8.png',
-			no: 'https://res.cloudinary.com/diplphrxm/image/upload/v1690692070/samples/plants/plant-fern_cgsdit.png',
+			yes: 'https://res.cloudinary.com/diplphrxm/image/upload/v1690692070/samples/plants/plant-fern_cgsdit.png',
+			no: 'https://res.cloudinary.com/diplphrxm/image/upload/v1690692068/samples/plants/plant-sansevieria_mnhug8.png',
+		},
+		inside_lot_indirect: {
+			yes: 'https://res.cloudinary.com/diplphrxm/image/upload/v1690692069/samples/plants/plant-monstera_zbntgh.png',
+			no: 'https://res.cloudinary.com/diplphrxm/image/upload/v1690692069/samples/plants/plant-aglaonema_gpsqr7.png',
 		},
 		outside: {
-			yes: 'https://res.cloudinary.com/diplphrxm/image/upload/v1690692069/samples/plants/plant-aloe_p4dp47.png',
-			no: 'https://res.cloudinary.com/diplphrxm/image/upload/v1690692069/samples/plants/plant-cactus_gntwe7.png',
+			yes: 'https://res.cloudinary.com/diplphrxm/image/upload/v1690692069/samples/plants/plant-cactus_gntwe7.png',
+			no: 'https://res.cloudinary.com/diplphrxm/image/upload/v1690692069/samples/plants/plant-aloe_p4dp47.png',
 		},
 	},
-	pets_answer: {
-		yes: 'https://res.cloudinary.com/diplphrxm/image/upload/v1690692068/samples/plants/soil-fertilized_mb43lu.png',
-		no: 'https://res.cloudinary.com/diplphrxm/image/upload/v1690692068/samples/plants/plant-sansevieria_mnhug8.png',
-	},
+	// pets_answer: {
+	// 	yes: 'https://res.cloudinary.com/diplphrxm/image/upload/v1690692068/samples/plants/soil-fertilized_mb43lu.png',
+	// 	no: 'https://res.cloudinary.com/diplphrxm/image/upload/v1690692068/samples/plants/plant-sansevieria_mnhug8.png',
+	// },
 	sunlight_answer: {
 		yes: 'https://res.cloudinary.com/diplphrxm/image/upload/v1690692067/samples/plants/soil-composted_qgfr4w.png',
-		no: 'https://res.cloudinary.com/diplphrxm/image/upload/v1690692067/samples/plants/soil-drainage_nsvcij.png',
+		no: 'https://res.cloudinary.com/diplphrxm/image/upload/v1690692068/samples/plants/soil-fertilized_mb43lu.png',
 	},
 	watering_answer: {
-		overwater:
-			'https://res.cloudinary.com/diplphrxm/image/upload/v1690692067/samples/plants/simple-clay-pot_tqarrp.png',
-		underwater:
-			'https://res.cloudinary.com/diplphrxm/image/upload/v1690692067/samples/plants/simple-ceramic-pot_dbvcsf.png',
-		neither:
-			'https://res.cloudinary.com/diplphrxm/image/upload/v1690692067/samples/plants/simple-ceramic-pot_dbvcsf.png',
+		overwater: {
+			minimalism: 'https://res.cloudinary.com/diplphrxm/image/upload/v1690692067/samples/plants/simple-clay-pot_tqarrp.png',
+			simple_colors: 'https://res.cloudinary.com/diplphrxm/image/upload/v1690692067/samples/plants/simple-clay-pot-decorated_zj9vij.png',
+			bright_colors: 'https://res.cloudinary.com/diplphrxm/image/upload/v1690692068/samples/plants/painted-clay-pot-decorated_eu1czn.png',
+		},
+		underwater: {
+			minimalism: 'https://res.cloudinary.com/diplphrxm/image/upload/v1690692067/samples/plants/simple-ceramic-pot_dbvcsf.png',
+			simple_colors: 'https://res.cloudinary.com/diplphrxm/image/upload/v1690692067/samples/plants/simple-ceramic-pot-decorated_na2fre.png',
+			bright_colors: 'https://res.cloudinary.com/diplphrxm/image/upload/v1690692068/samples/plants/painted-ceramic-pot-decorated_ntqod0.png',
+		},
+		neither: {
+			minimalism: 'https://res.cloudinary.com/diplphrxm/image/upload/v1690692067/samples/plants/simple-ceramic-pot_dbvcsf.png',
+			simple_colors: 'https://res.cloudinary.com/diplphrxm/image/upload/v1690692067/samples/plants/simple-ceramic-pot-decorated_na2fre.png',
+			bright_colors: 'https://res.cloudinary.com/diplphrxm/image/upload/v1690692068/samples/plants/painted-ceramic-pot-decorated_ntqod0.png',
+		}
 	},
 	style_answer: {
 		minimalism:
--- js/components/plant.-card.js
+++ js/components/plant.-card.js
@@ -2,22 +2,25 @@ import plantsOptions from '../../config.js';
 
 function getPlantData(data) {
 
+  console.table(data);
+
   const { pets, placement, style, sunlight, watering } = data;
   const {
     placement_answer,
-    pets_answer,
     sunlight_answer,
     watering_answer,
     style_answer,
     extra_elements_answer,
   } = plantsOptions;
 
-  console.log(pets_answer[pets], placement_answer[placement][pets], pets);
-
-  document.querySelector('.plant').innerHTML = `
-		<img src=${pets_answer[pets]}" alt="">
-		<img src="${placement_answer[placement][pets]}" alt="">
-   
+  // console.log(pets_answer[pets], '---------', placement_answer[placement][pets]);
+  // <img src="${pets_answer[pets]}" alt="">
+  // <img src="${style_answer[style]}" alt="">
+  document.querySelector('#plant').innerHTML = `
+ 
+  <img src="${watering_answer[watering][style]}" alt="">
+  <img src="${sunlight_answer[sunlight]}" alt="">
+  <img src="${placement_answer[placement][pets]}" alt="">
   `;
 }
 
