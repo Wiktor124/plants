896047a790ff9912720f30a28204ef680a5b2c6a
TROENCITY <porrasartaviajason@gmail.com>
2023-08-06 22:41:23 -0600

    update card recomendation

--- js/components/renderCardPlant.js
+++ js/components/renderCardPlant.js
@@ -1,23 +1,39 @@
+let currentCardContainer = null;
+
 function renderCard() {
-	const cardPlantsDiv = document.getElementById('card-plants');
+  const cardPlantsDiv = document.getElementById('card-plants');
 
-	const textPlants = document.createElement('p');
-	textPlants.textContent = 'The perfect plant fot you is...';
-	textPlants.classList.add('p-card');
+  if (!currentCardContainer) {
+    currentCardContainer = document.createElement('div');
+    currentCardContainer.classList.add('card-container'); 
+    cardPlantsDiv.appendChild(currentCardContainer);
+  }
 
-	cardPlantsDiv.appendChild(textPlants);
+  currentCardContainer.innerHTML = '';
 
-	const tituloPlants = document.createElement('h2');
-	tituloPlants.textContent = 'Titulo de planta';
-	tituloPlants.classList.add('title-plant');
+  const textPlants = document.createElement('p');
+  textPlants.textContent = 'The perfect plant for you is...';
+  textPlants.classList.add('p-card');
+  currentCardContainer.appendChild(textPlants);
 
-	cardPlantsDiv.appendChild(tituloPlants);
+  const tituloPlants = document.createElement('h2');
+  tituloPlants.textContent = 'Titulo de planta';
+  tituloPlants.classList.add('title-plant');
+  currentCardContainer.appendChild(tituloPlants);
 
-	const renderPlant = document.createElement('div');
-	renderPlant.id = 'plant';
-	renderPlant.className = 'plant';
+  const renderPlant = document.createElement('div');
+  renderPlant.id = 'plant';
+  renderPlant.className = 'plant';
+  currentCardContainer.appendChild(renderPlant);
 
-	cardPlantsDiv.innerHTML = renderPlant.outerHTML;
+	const textRecomendation = document.createElement('div');
+  textRecomendation.className = 'textRecomendation';
+  cardPlantsDiv.appendChild(textRecomendation);
+
+	const containerNameCard = document.createElement('div');
+  containerNameCard.className = 'containerNameCard';
+
+  textRecomendation.appendChild(containerNameCard);
+
 }
-
 export { renderCard };
