3a33410e1c80ac8224541b8cef6616bcbeadf0e9
TROENCITY <porrasartaviajason@gmail.com>
2023-08-08 17:43:52 -0600

    update

--- js/components/renderCardPlant.js
+++ js/components/renderCardPlant.js
@@ -14,26 +14,62 @@ function renderCard() {
   const textPlants = document.createElement('p');
   textPlants.textContent = 'The perfect plant for you is...';
   textPlants.classList.add('p-card');
-  currentCardContainer.appendChild(textPlants);
-
+  
   const tituloPlants = document.createElement('h2');
   tituloPlants.textContent = 'Titulo de planta';
   tituloPlants.classList.add('title-plant');
-  currentCardContainer.appendChild(tituloPlants);
-
+  
   const renderPlant = document.createElement('div');
   renderPlant.id = 'plant';
   renderPlant.className = 'plant';
-  currentCardContainer.appendChild(renderPlant);
-
+  
 	const textRecomendation = document.createElement('div');
   textRecomendation.className = 'textRecomendation';
-  cardPlantsDiv.appendChild(textRecomendation);
+  
+  // const namePlant = document.createElement('p');
+  // namePlant.textContent = 'Name : Mostera Deliciosa';
+  // namePlant.classList.add('namePlant');
 
-	const containerNameCard = document.createElement('div');
-  containerNameCard.className = 'containerNameCard';
+  // const soilPlant = document.createElement('p');
+  // soilPlant.textContent = 'Soil : Premium fertilized soil';
+  // soilPlant.classList.add('soilPlant');
 
-  textRecomendation.appendChild(containerNameCard);
+  // const potPlant = document.createElement('p');
+  // potPlant.textContent = 'Pot : Clay pot with decorations';
+  // potPlant.classList.add('potPlant');
 
+  // const colorPlant = document.createElement('p');
+  // colorPlant.textContent = 'Color : Clay';
+  // colorPlant.classList.add('colorPlant');
+
+  // const extrasPlant = document.createElement('p');
+  // extrasPlant.textContent = 'Extras : Moss pole';
+  // extrasPlant.classList.add('extrasPlant');
+  
+  currentCardContainer.appendChild(textPlants);
+  currentCardContainer.appendChild(tituloPlants);
+  currentCardContainer.appendChild(renderPlant);
+  currentCardContainer.appendChild(textRecomendation);
+
+  // textRecomendation.appendChild(namePlant);
+  // textRecomendation.appendChild(soilPlant);
+  // textRecomendation.appendChild(potPlant);
+  // textRecomendation.appendChild(colorPlant);
+  // textRecomendation.appendChild(extrasPlant);
+
+	const hola = document.createElement('div');
+  hola.className = 'hola';
+
+  const pato document.createElement('p');
+  pato.className = 'pato';
+
+  const vaca document.createElement('strong');
+  vaca.className = 'vaca';
+
+   
+  hola.appendChild(pato);
+  hola.appendChild(vaca);
+  textRecomendation.appendChild(hola);
+  
 }
 export { renderCard };
